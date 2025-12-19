d8c338d81bd5a42a5c732f1297c24779e101b7d9
sia shah <shahsia93@gmail.com>
2025-12-19 18:00:31 +0530

    Tables created successfully

--- src/main/java/com/repository/OfferProductDetailsRepo.java
+++ src/main/java/com/repository/OfferProductDetailsRepo.java
@@ -3,8 +3,10 @@ package com.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.entity.OfferProductDetailsEntity;
+
 @Repository
-public interface OfferProductDetailsRepo extends JpaRepository<OfferProductDetailsRepo, Integer>
+public interface OfferProductDetailsRepo extends JpaRepository<OfferProductDetailsEntity, Integer>
 {
 
 }
