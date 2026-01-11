b4eaaf37eb5a54cc7a210de715cb80b406430e6c
trushtihirapara2020-cmd <trushti.hirapara2020@gmail.com>
2026-01-11 23:00:06 +0530

    admin pages

--- src/main/java/com/controller/CustomerController.java
+++ src/main/java/com/controller/CustomerController.java
@@ -2,13 +2,14 @@ package com.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/customer")
 public class CustomerController 
 {
-	@GetMapping("/customer/dashboard")
-    public String customerDashboard() 
+	@GetMapping("/dashboard")
+    public String dashboard() 
 	{
         return "customer-dashboard";
     }
